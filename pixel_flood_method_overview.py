033a91bf975b24f0ac46abae495863a7d1efe3f9
Hannes Janott <hjanott@uos.de>
2024-09-19 22:07:30 +0200

    have overview and send script

--- pixel_flood_method_overview.py
+++ pixel_flood_method_overview.py
@@ -1,5 +1,9 @@
 from src import basis
 
+# get
+get_message(3, 2)
+
+# send
 basis.send_pixel_int(4,4,255,22,33)
 basis.send_pixel(4,4, "654312")
 basis.send_message("PX 5 5 12DF56\n")
---
+++ pixel_flood_send.py
@@ -0,0 +1,3 @@
+from src import basis
+
+basis.send_pixel_int(4,4,255,22,33)
