da01f318910663d9bf2583a1148f8b835433c187
Hannes Janott <hjanott@uos.de>
2024-09-19 21:52:26 +0200

    add rgb int send pixel

--- src/basis.py
+++ src/basis.py
@@ -2,8 +2,8 @@ import socket
 import time
 
 def send_message(message):
-    ip_address = "192.168.188.243"
-    port = 1337
+    ip_address = "192.168.188.106"
+    port = 1234
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((ip_address, port))
         s.sendall(message.encode())
@@ -12,3 +12,6 @@ def send_message(message):
 def send_pixel(x, y, rgb):
     message= f"PX {x} {y} " + rgb + "\n"
     send_message(message)
+    
+def send_pixel_int(x, y, r,g,b):
+    send_pixel(x,y,f"{r:X}{g:X}{b:X}")
